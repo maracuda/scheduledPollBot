8abe9be4085020cefafbb9304235c3da31d66035
bis <bis@skbkontur.ru>
2021-06-11 11:52:36 +0500

    Простое приложение для теста

---
+++ src/TelegramInteraction/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+using Vostok.Hosting;
+using Vostok.Hosting.Setup;
+
+namespace TelegramInteraction
+{
+    public static class EntryPoint
+    {
+        public static async Task Main()
+        {
+            var application = new ScheduledApplication();
+
+            void EnvironmentSetup(IVostokHostingEnvironmentBuilder builder)
+            {
+                builder
+                    .SetupApplicationIdentity(identityBuilder => identityBuilder
+                                                                 .SetProject("MyProject")
+                                                                 .SetApplication("MyApplication")
+                                                                 .SetEnvironment("Local")
+                                                                 .SetInstance("first")
+                                                                 )
+                    .SetupLog(logBuilder => logBuilder.SetupFileLog()
+                    );
+            }
+
+            var hostSettings = new VostokHostSettings(application, EnvironmentSetup);
+
+            var host = new VostokHost(hostSettings);
+
+            await host.WithConsoleCancellation().RunAsync();
+        }
+    }
+}
---
+++ src/TelegramInteraction/ScheduledApplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Vostok.Applications.Scheduled;
+using Vostok.Hosting.Abstractions;
+using Vostok.Logging.Abstractions;
+
+namespace TelegramInteraction
+{
+    public class ScheduledApplication : VostokScheduledAsyncApplication
+    {
+        protected override Task SetupAsync(IScheduledActionsBuilder builder, IVostokHostingEnvironment environment)
+        {
+            // Здесь следует регистрировать задачи через переданный builder.
+            // Для регистрации нужно передать имя, планировщик и делегат с полезной нагрузкой.
+            // Тут же можно провести и необходимый "прогрев" приложения.
+ 
+            builder.Schedule("action1", Scheduler.Periodical(TimeSpan.FromSeconds(10)),
+                             () =>
+                                 {
+                                     environment.Log.Info("I'm running!");
+                                 });
+         
+            return Task.CompletedTask;
+        }
+ }
+}

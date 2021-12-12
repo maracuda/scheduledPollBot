fc4e268dcd4cdc79158257793227735e5e7d5153
bis <bis@skbkontur.ru>
2021-12-12 21:15:25 +0500

    добавил обработку ошибок в нужное место

--- src/TelegramInteraction/Chat/ChatWorker.cs
+++ src/TelegramInteraction/Chat/ChatWorker.cs
@@ -14,12 +14,13 @@ namespace TelegramInteraction.Chat
     {
         public ChatWorker(ILog log,
                           ITelegramBotClient telegramBotClient,
-                          ICommandsRouter commandsRouter
+                          ICommandsRouter commandsRouter, ITelegramLogger telegramLogger
         )
         {
             this.log = log;
             bot = telegramBotClient;
             this.commandsRouter = commandsRouter;
+            this.telegramLogger = telegramLogger;
         }
 
         public async Task DoWorkAsync(CancellationToken cancellationToken)
@@ -27,7 +28,11 @@ namespace TelegramInteraction.Chat
             var me = await bot.GetMeAsync(cancellationToken);
             Console.Title = me.Username;
 
-            bot.OnReceiveError += (_, args) => log.Error(args.ApiRequestException);
+            bot.OnReceiveError += (_, args) =>
+                {
+                    log.Error(args.ApiRequestException);
+                    telegramLogger.Log(args.ApiRequestException);
+                };
             bot.OnUpdate += BotOnMessageReceived;
 
             bot.StartReceiving(new [] {UpdateType.Message, UpdateType.CallbackQuery}, cancellationToken);
@@ -36,12 +41,20 @@ namespace TelegramInteraction.Chat
 
         private async void BotOnMessageReceived(object? sender, UpdateEventArgs updateEventArgs)
         {
-            await commandsRouter.RouteAsync(updateEventArgs);
+            try
+            {
+                await commandsRouter.RouteAsync(updateEventArgs);
+            }
+            catch(Exception exception)
+            {
+                telegramLogger.Log(exception);
+            }
         }
 
         private readonly ILog log;
 
         private readonly ITelegramBotClient bot;
         private readonly ICommandsRouter commandsRouter;
+        private readonly ITelegramLogger telegramLogger;
     }
 }

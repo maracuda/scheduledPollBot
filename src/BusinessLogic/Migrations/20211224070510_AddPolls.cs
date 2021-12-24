616dbe34324309551e67bbf9afbaa2c8e50f0d17
bis <bis@skbkontur.ru>
2021-12-24 12:07:51 +0500

    миграция

---
+++ src/BusinessLogic/Migrations/20211224070510_AddPolls.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessLogic.Migrations
+{
+    public partial class AddPolls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedRequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Options = table.Column<string[]>(type: "text[]", nullable: true),
+                    IsAnonymous = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Schedule = table.Column<string>(type: "text", nullable: true),
+                    ChatId = table.Column<long>(type: "bigint", nullable: false),
+                    IsDisabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}

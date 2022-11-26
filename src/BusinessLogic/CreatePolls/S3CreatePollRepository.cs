1fa1b14a4f0f2e36f2c1592a14f24e88310044cb
maracuda <maracuda91@gmail.com>
2022-11-26 21:01:08 +0500

    Набросал репозиторий на ASW

---
+++ src/BusinessLogic/CreatePolls/S3CreatePollRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon.S3;
+
+using Newtonsoft.Json;
+
+namespace BusinessLogic.CreatePolls;
+
+public class S3CreatePollRepository : ICreatePollRepository
+{
+    public S3CreatePollRepository(
+        IAmazonS3 yandexAws
+    )
+    {
+        this.yandexAws = yandexAws;
+    }
+
+    public async Task CreateAsync(CreatePollRequest createPollRequest)
+    {
+        var allPolls = await ReadAllAsync();
+        await UploadAsync(allPolls.Append(createPollRequest).ToArray());
+    }
+
+    public async Task<CreatePollRequest[]> FindAsync(long userId)
+    {
+        var allPolls = await ReadAllAsync();
+        return allPolls.Where(p => p.UserId == userId).ToArray();
+    }
+
+    public async Task SaveAsync(CreatePollRequest pendingRequest)
+    {
+        var allPolls = (await ReadAllAsync()).ToDictionary(p => p.Id);
+
+        allPolls[pendingRequest.Id] = pendingRequest;
+
+        var allPollsValues = allPolls.Values.ToArray();
+        await UploadAsync(allPollsValues);
+    }
+
+    private async Task UploadAsync(CreatePollRequest[] allPollsValues)
+    {
+        var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(allPollsValues)));
+        await yandexAws.UploadObjectFromStreamAsync(bucketName,
+                                                    requests,
+                                                    memoryStream,
+                                                    new Dictionary<string, object>()
+        );
+    }
+
+    private async Task<CreatePollRequest[]> ReadAllAsync()
+    {
+        var response = await yandexAws.GetObjectAsync(bucketName, requests);
+        if(response.HttpStatusCode == HttpStatusCode.OK)
+        {
+            using(var reader = new StreamReader(response.ResponseStream))
+            {
+                var readToEndAsync = await reader.ReadToEndAsync();
+                return JsonConvert.DeserializeObject<CreatePollRequest[]>(readToEndAsync);
+            }
+        }
+
+        return Array.Empty<CreatePollRequest>();
+    }
+
+    private readonly IAmazonS3 yandexAws;
+    private static readonly string bucketName = "testpolls";
+    private static readonly string requests = "requests";
+}
---
+++ src/TelegramInteraction/YandexAswDependencyConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+
+using BusinessLogic;
+
+using SimpleInjector;
+
+namespace TelegramInteraction;
+
+public static class YandexAswDependencyConfigurator
+{
+    public static void Configure(this Container container, IApplicationSettings settings
+    )
+    {
+        var accessKey = settings.GetString("AwsAccessKey");
+        var secretKey = settings.GetString("AwsSecretKey");
+        var amazonS3Client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey), new AmazonS3Config
+            {
+                ServiceURL = "https://s3.yandexcloud.net",
+            });
+
+        container.RegisterInstance<IAmazonS3>(amazonS3Client);
+    }
+}

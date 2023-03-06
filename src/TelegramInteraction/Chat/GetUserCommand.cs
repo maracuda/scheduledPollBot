3d2c700daa10a92da226558f4a80c125f9c24473
bis <bis@skbkontur.ru>
2023-03-06 11:30:10 +0530

    команда, чтобы узнать пользователя

---
+++ src/TelegramInteraction/Chat/GetUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramInteraction.Chat;
+
+public class GetUserCommand : IChatCommand
+{
+    public GetUserCommand(ITelegramBotClient telegramBotClient)
+    {
+        this.telegramBotClient = telegramBotClient;
+    }
+
+    public bool CanHandle(Update update) =>
+        update?.Message?.Text != null && update.Message.Text.Contains("/userId");
+
+    public async Task ExecuteAsync(Update update)
+    {
+        var userId = long.Parse(update.Message.Text.Split(" ")[1]);
+
+        await telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id, $"tg://user?id={userId}");
+    }
+
+    private readonly ITelegramBotClient telegramBotClient;
+}
